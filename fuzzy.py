2f1bb12a373c5fb3f9cd26d89b82ab61aba2e4bb
Sirisak Lueangsaksri <spywhere@digitalparticle.com>
2015-05-02 04:47:01 +0700

    More improvements
    - Main app
     - Now show score nicely
     - Fix results size calculation
     - Collections now allow basic filtering
    - Fuzzy algorithm
     - Comments on weight
     - Remove remaining result from searching (there is no un-search-item in searching)
     - Calculate score function now return matched positions
     - Unmatch characters weight
     - Completion reward
     - Fix quick scoping not working properly

--- fuzzy.py
+++ fuzzy.py
@@ -2,14 +2,30 @@ import time
 from fuzzy import Fuzzy
 
 
-def generate_collections():
+def generate_collections(starts=""):
     with open("collections.txt", "r") as file:
-        return [line.strip() for line in file.readlines()]
+        return [
+            line.strip()
+            for line in file.readlines()
+            if line.startswith(starts)
+        ]
     return []
 
 
+def format_text(text, matches):
+    index = 0
+    output = ""
+    for c in text:
+        if index in matches:
+            output += c.upper()
+        else:
+            output += c
+        index += 1
+    return output
+
+
 def run():
-    limit = 20
+    limit = 10
     collection = generate_collections()
     fuzzy = Fuzzy(collection)
     print(fuzzy)
@@ -18,18 +34,24 @@ def run():
         if not search:
             return
         start_time = time.time()
-        result, remain = fuzzy.search(search)
+        result = fuzzy.search(search)
+        has_more = len(result) - limit
+        if has_more == 1:
+            limit += 1
         print("\"%s\" [%s of %s results (%.6f seconds)]" % (
             search,
+            min(limit, len(result)),
             len(result),
-            len(result) + remain,
             time.time() - start_time
         ))
-        has_more = len(result) > limit
         result = result[:limit]
         for text in result:
-            print("%s%s [%s]" % (" " * 2, text[1], text[0]))
-        if has_more:
+            print("%s[%8.2f] %s" % (
+                " " * 2,
+                text[0],
+                format_text(text[1], text[2])
+            ))
+        if has_more > 1:
             print(" " * 2 + "...")
 
 
--- fuzzy/fuzzy.py
+++ fuzzy/fuzzy.py
@@ -2,10 +2,20 @@ class Fuzzy:
     def __init__(self, collections=None):
         self.collections = collections or []
         self.weight = {
+            # + Matched character
             "match": 1,
+            # - Offset character
             "offset": 1,
+            # - Mismatched character
             "mismatch": 1,
-            "length": 0
+            # - Target > Source length
+            "length": 0,
+            # - Unmatched source character
+            "source_unmatch": 1,
+            # - Unmatched target character
+            "target_unmatch": 0.1,
+            # - Match completion reward
+            "complete_reward": 0
         }
         self.last_term = ""
         self.last_result = []
@@ -18,40 +28,46 @@ class Fuzzy:
 
         result = []
         for text in collections:
-            score = self.calculate_score(term, text)
+            score, matches = self.calculate_score(term, text)
             if score is None:
                 continue
-            result.append((score, text))
+            result.append((score, text, matches))
         result = sorted(result, key=lambda x: x[0], reverse=True)
 
-        total_result = len(result)
         self.last_result = []
         while len(result) > 0 and (limit <= 0 or len(self.last_result) < limit):
             self.last_result.append(result[0])
             result = result[1:]
-        return (self.last_result, total_result - len(self.last_result))
+        return self.last_result
 
     def calculate_score(self, term, text):
         if len(text) < len(term):
-            return None
+            return (None, [])
         score = (len(term) - len(text)) * self.weight["length"]
         source = 0
         target = 0
         last_match = 0
-        while source < len(term):
-            if term[source] and term[target:] not in text:
-                return None
+        matches = []
+        while source < len(term) and last_match < len(text):
+            if term[source] and term[source] not in text[last_match:]:
+                return (None, [])
             if term[source] == text[target]:
                 score += self.weight["match"]
                 score -= abs(source - target) * self.weight["offset"]
                 source += 1
+                matches.append(target)
                 last_match = target
-            score -= self.weight["mismatch"]
+            else:
+                score -= self.weight["mismatch"]
             target += 1
             if target >= len(text):
                 source += 1
                 target = last_match
-        return score
+        score -= (len(term) - source) * self.weight["source_unmatch"]
+        score -= (len(text) - last_match) * self.weight["target_unmatch"]
+        if last_match == len(text):
+            score += self.weight["complete_reward"]
+        return (score, matches)
 
     def __str__(self):
         return "Fuzzy [%s in collections]" % (len(self.collections))
